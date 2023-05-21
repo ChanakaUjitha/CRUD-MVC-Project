828386ba6b7f2b8098a948173c9ea7ee45bc6f58
Chanaka <Chanaka@CHANAKA>
2023-05-21 16:26:16 +0530

    add Web

---
+++ AuthSystem/Migrations/20230521104747_Initial crete.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AuthSystem.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initialcrete : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
